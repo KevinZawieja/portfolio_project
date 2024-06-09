40dc1c54582edde00102a435ce5b6415136b65cc
KevinZawieja <kevin_zawieja@hotmail.de>
2024-06-10 01:05:36 +0200

    -base

---
+++ src/components/home/Left.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { BsDownload } from "react-icons/bs";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FiActivity, FiMail } from "react-icons/fi";
+import Lebenslauf from "../../assets/Lebenslauf.pdf";
+import { bannerImg } from "../../assets/index";
+
+const Left = () => {
+  const [text] = useTypewriter({
+    words: [
+      "Haw Student",
+      "Bachelor of Science - Angewandte Informatik",
+      "Front-End & Back-End",
+    ],
+    loop: true,
+    typeSpeed: 30,
+    deleteSpeed: 20,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="w-full lgl:w-7/12 h-full bg-bodyColor rounded-2xl shadow-testShwdow z-10">
+      <div className="w-full h-3/5">
+        <img
+          className="w-full h-full object-cover object-top rounded-2xl"
+          src={bannerImg}
+          loading="priority"
+          alt="bannerImage"
+        />
+      </div>
+      <div className="w-full h-2/5 flex flex-col justify-between border-t-0 rounded-bl-xl rounded-br-xl">
+        <div className="flex flex-col items-center gap-2 py-10">
+          <h1 className="text-textColor text-4xl font-semibold">
+            Kevin Zawieja
+          </h1>
+          <p className="text-base text-designColor tracking-wide">
+            {text}
+            <Cursor cursorBlinking="false" cursorStyle="|" />
+          </p>
+          <div className="flex justify-center gap-2 mt-2">
+            <span
+              onClick={() =>
+                window.open("https://github.com/KevinZawieja", "_blank")
+              }
+              className="hover:text-designColor duration-300 cursor-pointer text-xl"
+            >
+              <FaGithub size={26} />
+            </span>
+            <span
+              onClick={() =>
+                window.open(
+                  "https://www.linkedin.com/in/kevin-zawieja-aab984214/",
+                  "_blank"
+                )
+              }
+              className="hover:text-designColor duration-300 cursor-pointer text-xl"
+            >
+              <FaLinkedin size={26} />
+            </span>
+            <span
+              onClick={() =>
+                (window.location.href = "mailto:kevin_zawieja@hotmail.de")
+              }
+              className="hover:text-designColor duration-300 cursor-pointer text-xl"
+            >
+              <FiMail size={26} />
+            </span>
+          </div>
+        </div>
+        <div className="flex h-14">
+          <a
+            href={Lebenslauf}
+            target="_blank"
+            className="w-1/2 border-t-[1px] borderRight border-t-zinc-800 text-sm tracking-wide uppercase  gap-2 hover:text-designColor duration-300"
+            rel="noreferrer"
+          >
+            <button className="w-full h-full flex justify-center items-center gap-2">
+              LEBENSLAUF <BsDownload />
+            </button>
+          </a>
+          <button className="w-1/2 border-t-[1px] border-t-zinc-800 text-sm tracking-wide uppercase flex justify-center items-center gap-2 hover:text-designColor duration-300">
+            PLATZHALTER <FiActivity />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Left;
---
+++ tailwind.config.js
@@ -0,0 +1,68 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
+      fontFamily: {
+        bodyfont: ["Poppins", "sans-serif"],
+        titleFont: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        bodyColor: "#212428",
+        textColor: "rgba(255, 255, 255, 0.85)",
+        designColor: "#6991c5", // #9cc0dc #113e8b #004e95 #4666a3
+        titleRoundBg:
+          "linear-gradient(135deg,rgba(120,204,109,.15) 0%,rgba(120,204,109,1%) 100%)",
+      },
+      animation: {
+        "spin-slow": "spin 15s linear infinite",
+        "reverse-spin": "reverse-spin 15s linear infinite",
+      },
+      keyframes: {
+        "reverse-spin": {
+          from: {
+            transform: "rotate(360deg)",
+          },
+        },
+      },
+      boxShadow: {
+        greenShadow: "0px 0px 188px -14px rgba(237,255,32,1)",
+        testShwdow: "11px 0px 13px -15px rgba(0,0,0,1)",
+      },
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".scrollbar-custom": {
+          /* Custom scrollbar (Webkit-based browsers) */
+          "&::-webkit-scrollbar": {
+            width: "10px",
+          },
+          "&::-webkit-scrollbar-track": {
+            background: "transparent",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            background: "#6991c5" /* Adjust the color as needed */,
+            borderRadius: "9999px",
+          },
+          "&::-webkit-scrollbar-thumb:hover": {
+            background: "rgb(31 29 29, 0.8)" /* Adjust the color as needed */,
+          },
+        },
+      };
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
+};
